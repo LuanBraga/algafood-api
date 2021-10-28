db944f02034125c92bd71cf9fc3e2df6e98bf1c1
LuanBraga <luanbraga.lb@gmail.com>
2021-10-28 10:01:20 -0300

    Create repository folder / CozinhaRepository interface

---
+++ src/main/java/com/algaworks/algafoodapi/domain/model/repository/CozinhaRepository.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafoodapi.domain.model.repository;
+
+import java.util.List;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+
+public interface CozinhaRepository {
+    
+    List<Cozinha> listar();
+
+    Cozinha buscar(Long id);
+
+    Cozinha salvar(Cozinha cozinha);
+
+    void remover(Cozinha cozinha);
+}
