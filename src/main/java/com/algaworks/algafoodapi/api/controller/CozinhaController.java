7a96b68dbd44204ccc52de0ba77fb40dfc263acd
LuanBraga <luanbraga.lb@gmail.com>
2021-11-17 11:17:43 -0300

    add cozinha/estado controller

---
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import java.util.List;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+}
