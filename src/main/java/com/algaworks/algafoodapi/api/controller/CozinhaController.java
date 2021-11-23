dcc1edb4d2fc89f5159e6d6ed5b79c3fa419d060
Berserker <luanbraga.lb@gmail.com>
2021-11-22 22:54:22 -0300

    A man who stands for nothing will fall for anything

--- src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -2,12 +2,16 @@ package com.algaworks.algafoodapi.api.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +24,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id){
+        return cozinhaRepository.buscar(id);
     }
 }
--- src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
@@ -7,6 +7,7 @@ import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class EstadoController {
     @GetMapping
     public List<Estado> listar(){
         return estadoRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Estado buscar(@PathVariable Long id){
+        return estadoRepository.buscar(id);
     }
 }
